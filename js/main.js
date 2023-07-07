0feadf116aea3e8c943c7fe812c7b2ce8a84988b
carla-alcala <carlaalcala29@gmail.com>
2023-07-07 16:11:06 -0300

    js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
-const nav = document.querySelector (#nav);
-const abrir = document.querySelector (#abrir);
-const cerrar = document.querySelector(#cerrar);
+const nav = document.querySelector ("#nav");
+const abrir = document.querySelector ("#abrir");
+const cerrar = document.querySelector("#cerrar");
 
-abrir.addEventListerner("click", ()=> {
+abrir.addEventListerner("click" , () => {
     nav.classList.add("visible");
 })
 
-cerrar.addEventListerner ("click", ()=> {
+cerrar.addEventListerner ("click" , () => {
     nav.classList.remove ("visible");
 })
+let num1 = 20;
+let num2 = 7;
+let resultado = num1 + num2;
+console:log(resultado);
